935d7dd00b2e3b620af823f4557c3860aef7ad8d
JR1811 <janreichelt77@gmail.com>
2023-04-05 05:56:43 +0200

    personalized custom AcanthusPocket mod

---
+++ src/main/java/net/shirojr/AcanthusPocket.java
@@ -0,0 +1,15 @@
+package net.shirojr;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AcanthusPocket implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("acanthus-pocket");
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("");
+	}
+}
